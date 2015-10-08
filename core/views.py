334c8ccfb04e4d8aec6e3910a7b736a1d09d7b56
SergeyPod <spodolsky@mail.ru>
2015-10-08 13:25:03 -0400

    small cleanup

--- core/views.py
+++ core/views.py
@@ -3617,8 +3617,29 @@ def taskList(request):
     tquery = {}
     tquery['jeditaskid__in'] = tasksIdToBeDisplayed
     tasksEventInfo = GetEventsForTask.objects.filter(**tquery).values('jeditaskid','totevrem', 'totev')
-    failedInScouting = JediDatasets.objects.filter(**tquery).extra(where=['nFiles > nFilesTobeUsed']).values('jeditaskid')
-    failedInScouting = [ item['jeditaskid'] for item in failedInScouting]
+    failedInScouting = JediDatasets.objects.filter(**tquery).extra(where=['NFILESFAILED > NFILESTOBEUSED']).values('jeditaskid')
+
+    taskStatuses = dict((task['jeditaskid'], task['status']) for task in tasks)
+
+    failedInScouting = [ item['jeditaskid'] for item in failedInScouting if (taskStatuses[item['jeditaskid']] not in ('scouting'))]
+
+    #scoutingHasCritFailures
+    tquery['nfilesfailed__gt'] = 0
+    scoutingHasCritFailures = JediDatasets.objects.filter(**tquery).values('jeditaskid')
+    scoutingHasCritFailures = [ item['jeditaskid'] for item in scoutingHasCritFailures if (taskStatuses[item['jeditaskid']] in ('scouting'))]
+
+    tquery = {}
+    tquery['nfilesfailed'] = 0
+    tquery['jeditaskid__in'] = tasksIdToBeDisplayed
+    scoutingHasNonCritFailures = JediDatasets.objects.filter(**tquery).values('jeditaskid')
+    scoutingHasNonCritFailures = [ item['jeditaskid'] for item in scoutingHasNonCritFailures if (taskStatuses[item['jeditaskid']] == 'scouting' and item['jeditaskid'] not in scoutingHasCritFailures )]
+
+    tquery = {}
+    tquery['jeditaskid__in'] = scoutingHasNonCritFailures
+    tquery['relationtype'] = 'retry'
+    scoutingHasNonCritFailures = JediJobRetryHistory.objects.filter(**tquery).values('jeditaskid')
+    scoutingHasNonCritFailures = [ item['jeditaskid'] for item in scoutingHasNonCritFailures]
+
     for task in taskslToBeDisplayed:
         correspondendEventInfo = filter(lambda n: n.get('jeditaskid') == task['jeditaskid'], tasksEventInfo)
         if len(correspondendEventInfo) > 0:
@@ -3628,9 +3649,12 @@ def taskList(request):
             task['totevrem'] = 0
             task['totev'] = 0
         if (task['jeditaskid'] in failedInScouting):
-                task['failedscouting'] = True
-        else:
-            task['failedscouting'] = False
+            task['failedscouting'] = True
+        if (task['jeditaskid'] in scoutingHasCritFailures):
+            task['scoutinghascritfailures'] = True
+        if (task['jeditaskid'] in scoutingHasNonCritFailures):
+            task['scoutinghasnoncritfailures'] = True
+
 
     ## For event service, pull the jobs and event ranges
     if eventservice:        
