d6fc1f98f467943600fefd3cfd577a42e95e8f8f
podolsky <spodolsky@mail.ru>
2015-11-16 10:21:23 +0100

    added jobs summary information to JSON on the /jobs/ page

--- core/views.py
+++ core/views.py
@@ -1708,7 +1708,7 @@ def jobList(request, mode=None, param=None):
                         del job[field]
 
         data = {
-            "selectionsummury": sumd,
+            "selectionsummary": sumd,
             "jobs": jobs
         }
 
