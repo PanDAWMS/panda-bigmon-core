6a5d55277f63601a89eb5b6aaf9f05b4de074a06
tkorchug <tatiana.korchuganova@cern.ch>
2016-02-22 17:42:41 +0100

    added runningprodtasks page

--- core/urls.py
+++ core/urls.py
@@ -51,7 +51,7 @@ urlpatterns = patterns('',
 
     url(r'^g4exceptions/$', coremon_views.g4exceptions, name='g4exceptions'),
     url(r'^worldjobs/$', coremon_views.worldjobs, name='worldjobs'),
-
+    url(r'^runningprodtasks/$', coremon_views.runningProdTasks, name='runningProdTasks'),
 
 
 #    url(r'^preprocessdata/$', coremon_views.preprocessData, name='preprocessdata'),
