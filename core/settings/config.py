40d233be1e61b3279b4bfde79f55c49b9107541a
tkorchug <tatiana.korchuganova@cern.ch>
2016-02-23 12:56:01 +0100

    Revert "Merge remote branch 'upstream/master'"
    This reverts commit f6d0d933493cec1192c14ae96072b6cde71fc686, reversing
    changes made to edd712168fb3743ef2b7877decb30bf50766f9dc.

--- core/settings/config.py
+++ core/settings/config.py
@@ -117,7 +117,7 @@ FILTER_UI_ENV = {
 
 #LOG_ROOT = '/data/bigpandamon_virtualhosts/core/logs'
 #LOG_ROOT = '/data/wenaus/logs'
-LOG_ROOT = '/afs/cern.ch/user/t/tkorchug/PycharmProjects/panda-bigmon-core/log'
+LOG_ROOT = '/data/wenaus/bigpandamon_virtualhosts/twrpm/logs'
 LOG_SIZE = 1000000000
 LOGGING = {
     'version': 1,
