d287f0d43f4783ea24d35e46a363313a15c3c558
spodolsky@mail.ru <spodolsky@mail.ru>
2017-05-04 14:17:49 -0400

    updated obsolete reports

--- core/reports/ObsoletedTasksReport.py
+++ core/reports/ObsoletedTasksReport.py
@@ -8,12 +8,35 @@ class ObsoletedTasksReport:
     def __init__(self):
         pass
 
-    def prepareReport(self, request):
-        """
-        1. SELECT TASKID,PPFLAG FROM ATLAS_DEFT.T_PRODUCTION_TASK WHERE PPFLAG > 0
-        2. Select where ChainID = taskid
-        3. Select MAX DATE FROM DATASET, status=deleted
-        """
+    def prepareReportTasksV1(self, request, type):
+        sqlRequest = '''
+                    SELECT * FROM (
+            WITH RECONSTRUCTEDTASKCHAIN AS (
+            SELECT TASKID, PR_ID, TASKNAME, CHAIN_TID, PARENT_TID, STATUS as TASKSTATUS, LEVEL as LEV, PPFLAG, CASE WHEN PPGRACEPERIOD = -1 THEN 48 ELSE PPGRACEPERIOD END as PPGRACEPERIOD FROm ATLAS_DEFT.T_PRODUCTION_TASK 
+            START WITH PPFLAG > 0
+            CONNECT BY NOCYCLE PRIOR TASKID=PARENT_TID ORDER SIBLINGS BY TASKID
+            ) SELECT RECONSTRUCTEDTASKCHAIN.*, STATUS as DSSTATUS, TIMESTAMP, row_number() OVER(PARTITION BY RECONSTRUCTEDTASKCHAIN.TASKID order by t_production_dataset.TIMESTAMP) AS PART, t_production_dataset.NAME as dsname FROM ATLAS_DEFT.RECONSTRUCTEDTASKCHAIN, ATLAS_DEFT.t_production_dataset  WHERE t_production_dataset.TASKID=RECONSTRUCTEDTASKCHAIN.TASKID
+            and instr(t_production_dataset.NAME,'.log.') = 0 
+            ) WHERE PART=1 and PPFLAG>=0 ORDER BY LEV ASC
+        '''
+
+        cur = connection.cursor()
+        cur.execute(sqlRequest)
+        stats = cur.fetchall()
+        tasksInfoList = []
+        for taskEntry in stats:
+            tmpDict = {"reqid": taskEntry[1], "taskid": taskEntry[0], "taskname": taskEntry[2], "dsname": taskEntry[12]}
+            tasksInfoList.append(tmpDict)
+        data = {}
+        data['tasksInfo'] = tasksInfoList
+        data['built'] = datetime.now().strftime("%d %b %Y %H:%M:%S")
+        data['type'] = type
+        return render_to_response('reportObsoletedTasksv2.html', data, RequestContext(request))
+
+
+
+
+    def prepareReportTasksV0(self, request):
 
         sqlRequest = '''
             SELECT * FROM (
@@ -25,13 +48,11 @@ class ObsoletedTasksReport:
             and instr(t_production_dataset.NAME,'.log.') = 0 
             ) WHERE PART=1 and PPFLAG>=0 ORDER BY LEV ASC
         '''
-
         cur = connection.cursor()
         cur.execute(sqlRequest)
         stats = cur.fetchall()
         tasksInfo = OrderedDict()
         inversedMap = {}
-
         for taskEntry in stats:
             if taskEntry[4] == 1: #This is entry level of tasks chain
                 if taskEntry[5] == 1:
@@ -42,8 +63,6 @@ class ObsoletedTasksReport:
                 tmpDict["graceperiod"] = taskEntry[6]
                 tmpDict["dsname"] = taskEntry[10]
                 tmpDict["dsstatus"] = taskEntry[3]
-
-
                 tasksInfo[taskEntry[0]] = tmpDict
             else:
                 if taskEntry[2] in inversedMap: #here we check if parent task already assigned
@@ -57,16 +76,22 @@ class ObsoletedTasksReport:
                 tempDic["date"] = taskEntry[8]
                 ### If not deleted we should add graceperiod to date
                 tasksInfo[inversedMap[taskEntry[0]]] = tempDic
-
         tasksInfo = sorted(tasksInfo.iteritems(), key=lambda x: x[1]['date'], reverse=True)
         tasksInfoList = []
         for (key, value) in tasksInfo:
             value['date'] = value['date'].strftime("%d %b %Y %H:%M:%S")
             value['rootTask'] = key
             tasksInfoList.append(value)
-
         data = {}
         data['tasksInfo'] = tasksInfoList
         data['built'] = datetime.now().strftime("%d %b %Y %H:%M:%S")
-
         return render_to_response('reportObsoletedTasks.html', data, RequestContext(request))
+
+
+    def prepareReport(self, request):
+        if 'obstasks' in request.session['requestParams'] and request.session['requestParams']['obstasks'] == 'tasksview':
+            return self.prepareReportTasksV1(request, "tasksview")
+        elif 'obstasks' in request.session['requestParams'] and request.session['requestParams']['obstasks'] == 'dsview':
+            return self.prepareReportTasksV1(request, "dsview")
+        else:
+            return self.prepareReportTasksV0(request)
