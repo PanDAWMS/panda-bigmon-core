5ce9b59afe2b33583abd36877c4feeac327a267b
podolsky <spodolsky@mail.ru>
2017-06-06 17:11:12 +0200

    added suppression of killed and lost heartbeat ES jobs

--- core/views.py
+++ core/views.py
@@ -152,6 +152,8 @@ LAST_N_HOURS_MAX = 0
 PLOW = 1000000
 PHIGH = -1000000
 
+DEFAULTFAILEDJOBSSUPPRESSIONRUNTINE = 10
+
 standard_fields = ['processingtype', 'computingsite', 'jobstatus', 'prodsourcelabel', 'produsername', 'jeditaskid',
                    'workinggroup', 'transformation', 'cloud', 'homepackage', 'inputfileproject', 'inputfiletype',
                    'attemptnr', 'specialhandling', 'priorityrange', 'reqid', 'minramcount', 'eventservice',
@@ -5844,6 +5846,7 @@ def dashboard(request, view='production'):
         if 'hours' in request.session['requestParams']:
             hours = int(request.session['requestParams']['hours'])
 
+        extra = '(1=1)'
 
         if view == 'production':
             query['tasktype'] = 'prod'
@@ -5853,6 +5856,17 @@ def dashboard(request, view='production'):
         if 'es' in request.session['requestParams'] and request.session['requestParams']['es'].upper() == 'TRUE':
             query['es'] = 1
             estailtojobslinks = '&eventservice=eventservice'
+
+            if not 'notsuppress' in request.session['requestParams']:
+                suppressruntime = DEFAULTFAILEDJOBSSUPPRESSIONRUNTINE
+                if 'suppressruntime' in request.session['requestParams']:
+                    try:
+                        suppressruntime = int(request.session['requestParams'])
+                    except:
+                        pass
+                extra = '( not( (JOBDISPATCHERERRORCODE=100 OR PILOTERRORCODE=1201) and ((ENDTIME-STARTTIME)*24*60 < '+str(suppressruntime)+')))'
+
+
         if 'es' in request.session['requestParams'] and request.session['requestParams']['es'].upper() == 'FALSE':
             query['es'] = 0
 
@@ -5862,8 +5876,8 @@ def dashboard(request, view='production'):
         jobsarch4statuses = ['finished', 'failed', 'cancelled', 'closed']
         if ('modificationtime__range' in excludedTimeQuery and not 'date_to' in request.session['requestParams']):
             del excludedTimeQuery['modificationtime__range']
-        worldJobsSummary.extend(CombinedWaitActDefArch4.objects.filter(**excludedTimeQuery).values(*values).exclude(isarchive=1).annotate(countjobsinstate=Count('jobstatus')))
-        worldJobsSummary.extend(CombinedWaitActDefArch4.objects.filter(**query).values(*values).exclude(isarchive=0).annotate(countjobsinstate=Count('jobstatus')))
+        worldJobsSummary.extend(CombinedWaitActDefArch4.objects.filter(**excludedTimeQuery).values(*values).extra(where=[extra]).exclude(isarchive=1).annotate(countjobsinstate=Count('jobstatus')))
+        worldJobsSummary.extend(CombinedWaitActDefArch4.objects.filter(**query).values(*values).extra(where=[extra]).exclude(isarchive=0).annotate(countjobsinstate=Count('jobstatus')))
         nucleus = {}
         statelist1 = statelist
         #    del statelist1[statelist1.index('jclosed')]
@@ -7608,11 +7622,21 @@ def taskInfo(request, jeditaskid=0):
             if 'mode' in request.session['requestParams'] and request.session['requestParams'][
                 'mode'] == 'nodrop': mode = 'nodrop'
 
+            extra = "(1=1)"
+            if not 'notsuppress' in request.session['requestParams']:
+                suppressruntime = DEFAULTFAILEDJOBSSUPPRESSIONRUNTINE
+                if 'suppressruntime' in request.session['requestParams']:
+                    try:
+                        suppressruntime = int(request.session['requestParams'])
+                    except:
+                        pass
+                extra = '( not( (JOBDISPATCHERERRORCODE=100 OR PILOTERRORCODE=1201) and ((ENDTIME-STARTTIME)*24*60 < '+str(suppressruntime)+')))'
+
 
             plotsDict, jobsummary, eventssummary, transactionKey, jobScoutIDs, hs06sSum = jobSummary2(
-                query, exclude={}, mode=mode, isEventServiceFlag=True, substatusfilter='non_es_merge')
+                query, exclude={}, extra=extra, mode=mode, isEventServiceFlag=True, substatusfilter='non_es_merge')
             plotsDictESMerge, jobsummaryESMerge, eventssummaryESM, transactionKeyESM, jobScoutIDsESM, hs06sSumESM = jobSummary2(
-                query, exclude={}, mode=mode, isEventServiceFlag=True, substatusfilter='es_merge')
+                query, exclude={}, extra=extra, mode=mode, isEventServiceFlag=True, substatusfilter='es_merge')
             for state in eventservicestatelist:
                 eventstatus = {}
                 eventstatus['statusname'] = state
@@ -8021,7 +8045,7 @@ def ganttTaskChain(request):
     patch_response_headers(response, cache_timeout=request.session['max_age_minutes'] * 60)
     return response
 
-def jobSummary2(query, exclude={}, mode='drop', isEventServiceFlag=False, substatusfilter='', processingtype=''):
+def jobSummary2(query, exclude={}, extra = "(1=1)", mode='drop', isEventServiceFlag=False, substatusfilter='', processingtype=''):
     jobs = []
     jobScoutIDs = {}
     jobScoutIDs['cputimescoutjob'] = []
@@ -8049,12 +8073,12 @@ def jobSummary2(query, exclude={}, mode='drop', isEventServiceFlag=False, substa
 
     # print str(datetime.now())
 
-    jobs.extend(Jobsarchived.objects.filter(**newquery).exclude(**exclude).values(*values))
+    jobs.extend(Jobsarchived.objects.filter(**newquery).extra(where=[extra]).exclude(**exclude).values(*values))
 
-    jobs.extend(Jobsdefined4.objects.filter(**newquery).exclude(**exclude).values(*values))
-    jobs.extend(Jobswaiting4.objects.filter(**newquery).exclude(**exclude).values(*values))
-    jobs.extend(Jobsactive4.objects.filter(**newquery).exclude(**exclude).values(*values))
-    jobs.extend(Jobsarchived4.objects.filter(**newquery).exclude(**exclude).values(*values))
+    jobs.extend(Jobsdefined4.objects.filter(**newquery).extra(where=[extra]).exclude(**exclude).values(*values))
+    jobs.extend(Jobswaiting4.objects.filter(**newquery).extra(where=[extra]).exclude(**exclude).values(*values))
+    jobs.extend(Jobsactive4.objects.filter(**newquery).extra(where=[extra]).exclude(**exclude).values(*values))
+    jobs.extend(Jobsarchived4.objects.filter(**newquery).extra(where=[extra]).exclude(**exclude).values(*values))
 
     # print str(datetime.now())
 
