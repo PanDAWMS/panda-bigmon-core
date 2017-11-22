ae1a78b9d16d63841c24b7f4c94046677635b742
tkorchug <tatiana.korchuganova@cern.ch>
2017-11-22 19:13:35 +0700

    Added save result to db table function

--- core/art/jobSubResults.py
+++ core/art/jobSubResults.py
@@ -26,6 +26,21 @@ def getJobReport(guid, lfn, scope):
         url = urlBase + "/" + f['name']
         response = http.request('GET', url)
         data = json.loads(response.data)
+
+    return data
+
+def getARTjobSubResults(data):
+    jobSubResult = {}
     if 'art' in data:
         jobSubResult = data['art']
+
+    # protection of json format change from list to list of dicts
+    if 'result' in jobSubResult and isinstance(jobSubResult['result'], list):
+        resultlist = []
+        for r in jobSubResult['result']:
+            if not isinstance(r, dict):
+                resultlist.append({'name': '', 'result': r})
+            else:
+                resultlist.append({'name': r['name'] if 'name' in r else '', 'result': r['result'] if 'result' in r else r})
+        jobSubResult['result'] = resultlist
     return jobSubResult
