05d07cede9ffac4aed02b505a35319cee3932c61
Alekeev <frt@tpu.ru>
2018-03-05 15:50:53 +0100

    Fixed statpixel

--- core/views.py
+++ core/views.py
@@ -11855,8 +11855,7 @@ def statpixel(request):
 
     #this is a transparent gif pixel
     pixel_= "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b"
-    responce = HttpResponse(pixel_, content_type='image/gif')
-    response.delete_cookie("sessionid")
+    response = HttpResponse(pixel_, content_type='image/gif')
     return response
 
 #@cache_page(60 * 20)
