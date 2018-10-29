f00cdd74b6204f45537f6cbeb5722b33cde3fed6
tkorchug <tatiana.korchuganova@cern.ch>
2018-10-29 16:04:06 +0100

    turn off caching for taskInfoNew

--- core/views.py
+++ core/views.py
@@ -8530,9 +8530,9 @@ def taskInfoNew(request, jeditaskid=0):
         data = getCacheEntry(request, tkiec, isData=True)
         return HttpResponse(data, content_type='text/html')
 
-    # data = None
-    data = getCacheEntry(request, "taskInfoNew", skipCentralRefresh=True)
 
+    data = getCacheEntry(request, "taskInfoNew", skipCentralRefresh=True)
+    data = None
     # Temporary protection
     if data is not None:
         data = json.loads(data)
