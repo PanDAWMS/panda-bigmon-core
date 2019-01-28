50d6a75e6265d53743ca847f33ec8adbece6e281
Sergey Padolski <spodolsky@mail.ru>
2019-01-28 13:39:43 -0500

    added corrupted event status to the main view

--- core/views.py
+++ core/views.py
@@ -173,7 +173,7 @@ statelist = ['defined', 'waiting', 'pending', 'assigned', 'throttled',
              'transferring', 'finished', 'failed', 'cancelled', 'merging', 'closed']
 sitestatelist = ['defined', 'waiting', 'assigned', 'throttled', 'activated', 'sent', 'starting', 'running', 'holding',
                  'merging', 'transferring', 'finished', 'failed', 'cancelled']
-eventservicestatelist = ['ready', 'sent', 'running', 'finished', 'cancelled', 'discarded', 'done', 'failed', 'fatal','merged']
+eventservicestatelist = ['ready', 'sent', 'running', 'finished', 'cancelled', 'discarded', 'done', 'failed', 'fatal','merged', 'corrupted']
 taskstatelist = ['registered', 'defined', 'assigning', 'ready', 'pending', 'scouting', 'scouted', 'running', 'prepared',
                  'done', 'failed', 'finished', 'aborting', 'aborted', 'finishing', 'topreprocess', 'preprocessing',
                  'tobroken', 'broken', 'toretry', 'toincexec', 'rerefine']
