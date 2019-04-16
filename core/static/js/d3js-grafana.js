c571ed3fb9563677faef95378b0a77e9872f8f45
tkorchug <tatiana.korchuganova@cern.ch>
2019-04-16 13:06:46 +0200

    Updated plots from grafana API

--- core/static/js/d3js-grafana.js
+++ core/static/js/d3js-grafana.js
@@ -4,55 +4,54 @@ function stackedHistOrdinal(data, divToShow, title) {
     var colors = ["#116aff", "#fe8504", "#1ff7fe", "#f701ff", "#2e4a02", "#ffaad5", "#f1ff8d", "#1eff06", "#700111", "#1586c3", "#ff067d", "#0e02fb", "#1bffa1", "#921e8f", "#c49565", "#fd0128", "#4ea105", "#158279", "#c8fe0a", "#fdcc0b", "#834969", "#ff7673", "#05018b", "#c591fe", "#a6d8ab", "#948c01", "#484ba1", "#fe22c0", "#06a05d", "#694002", "#8e39e9", "#bdc6ff", "#030139", "#b33802", "#85fa60", "#a2025b", "#3e021b", "#ffcd6d", "#4a92ff", "#e564b6", "#43cfff", "#7e9051", "#e768fc", "#09406b", "#b17005", "#8fd977", "#c1063e", "#a7594f", "#14e3b8", "#bccb1e", "#53064f", "#fff1b7", "#997dba", "#fe965c", "#ffb0a7", "#046c04", "#8451ce", "#d46585", "#fef70c", "#1003c3", "#024a2e", "#0fc551", "#1f025d", "#fd5302", "#5bbfc4", "#481903", "#bfc066", "#ad04bb", "#efa425", "#06c709", "#9701ff", "#84468e", "#018da8", "#88cf01", "#6d6412", "#658a1d", "#0d3cb4", "#144cfe", "#fe5d43", "#33753e", "#4cb28f", "#e6b4ff", "#a5feef", "#caff68", "#d80f8a", "#79193a", "#97fdba", "#a85726", "#fe8cf9", "#8bfe01", "#4a315d", "#ff0155", "#02ff5e", "#6b0199", "#bc7e9f", "#fde75c"];
 
     var formatCount = d3.format(",.0f");
-    var formatXAxis = d3.format(".2s");
+    var formatYAxis = d3.format(".2s");
     var formatStats = d3.format(".3s");
 
-    var margin = {top: 30, right: 100, bottom: 100, left: 70},
-        width = 650 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    var margin = {top: 30, right: 100, bottom: 200, left: 70},
+        width = 1024 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
 
-    var values = data['sites'];
-    var ranges = data['ranges'];
-    var stats = data['stats'];
 
-    var statistics = [{type: "\u03BC", val:formatStats(stats[0])}];
-	statistics.push({type:"\u03C3", val:formatStats(stats[1])});
+    var x_keys = Object.keys(data);
 
-    var lowerBand = ranges[0];
-    var upperBand = ranges[ranges.length - 1];
+    var fields = [];
+    Object.keys(data).forEach(function (key) {
+        Object.keys(data[key]).forEach(function (keyi) {
+            if (fields.indexOf(keyi) < 0) {
+                fields.push(keyi)
+            }
+        })
+    });
+
+    // var statistics = [{type: "\u03BC", val:formatStats(stats[0])}];
+	// statistics.push({type:"\u03C3", val:formatStats(stats[1])});
 
 
     var x = d3.scale.ordinal()
-        .domain(ranges)
-        .rangeRoundPoints([0, width]);
+        .domain(x_keys)
+        .rangeRoundBands([0, width], 0.1);
 
-    numberofbins = ranges.length - 1;
+    var numberofbins = x_keys.length - 1;
     var filterBase = 1;
 
     var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom")
-        .tickValues(ranges.map( function(d,i) {
-            if(i % filterBase === 0 ) {
-                return d;
-              }
-            })
-            .filter(function (d)
-            {
-                return !!d;
-            }
-            ))
-        .tickFormat(formatXAxis);
+        .tickValues(x_keys);
     var y = d3.scale.linear().range([height, 0]);
     var yAxis = d3.svg.axis().scale(y)
-        .orient("left");
+        .orient("left")
+        .tickFormat(formatYAxis);
 
     var intermediateData = [];
+    fields.forEach(function (v) {
+        intermediateData.push({field:v, values: []})
+    });
+
     var layer = 0;
-    Object.keys(values).forEach(function(key) {
-        intermediateData.push({site:key, values: []});
-        values[key].forEach(function (d,i) {
-            intermediateData[layer].values.push({x: x(ranges[i]), y:parseInt(d,10)})
+    Object.keys(data).forEach(function(key) {
+        Object.keys(data[key]).forEach(function (keyi) {
+            intermediateData[fields.indexOf(keyi)].values.push({x: x(key), y:parseInt(data[key][keyi],10)})
         });
         layer +=1;
     });
@@ -80,15 +79,15 @@ function stackedHistOrdinal(data, divToShow, title) {
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    var bin = svg.selectAll(".site")
+    var bin = svg.selectAll(".field")
         .data(stackedData)
         .enter().append("g")
-        .attr("class", "site")
+        .attr("class", "field")
         .style("fill", function (d, i) {
-            return color(d.site);
+            return color(d.field);
         })
         .style("stroke", function (d, i) {
-            return d3.rgb(color(d.site)).darker();
+            return d3.rgb(color(d.field)).darker();
         })
         .style("stroke-width", 0.4);
 
@@ -113,32 +112,13 @@ function stackedHistOrdinal(data, divToShow, title) {
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
         .selectAll("text")
-        .attr("dx", -32)
-        .attr("dy", 5)
+        .style("text-anchor", "end")
+        .attr("dx", -10)
+        .attr("dy", 0)
         .attr("transform", function (d) {
-            return "rotate(-45)"
+            return "rotate(-65)"
         });
-    if (title.indexOf('PSS') >= 0) {
-        svg.append("g")
-            .attr("transform", "translate(" + (width + 10) + " ," + (height + 15) + ")")
-            .append("text")
-            .style("text-anchor", "left")
-            .text("PSS, MB");
-    }
-    if (title.indexOf('Walltime') >= 0) {
-        svg.append("g")
-            .attr("transform", "translate(" + (width + 10) + " ," + (height + 15) + ")")
-            .append("text")
-            .style("text-anchor", "left")
-            .text("Time, s");
-    }
-    if (title.indexOf('HS06') >= 0) {
-        svg.append("g")
-            .attr("transform", "translate(" + (width + 10) + " ," + (height + 15) + ")")
-            .append("text")
-            .style("text-anchor", "left")
-            .text("HS06s");
-    }
+
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
@@ -149,7 +129,7 @@ function stackedHistOrdinal(data, divToShow, title) {
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
-        .text("N jobs");
+        .text("Sum of CPU consumption");
 
     svg.append("g")
         .attr("transform", "translate(" + (width / 2) + ", -10)")
@@ -158,6 +138,7 @@ function stackedHistOrdinal(data, divToShow, title) {
         .text(title);
 
     var squareside = 10;
+    const yaxisheight = 6*Math.max.apply(Math, x_keys.map(function (el) {return el.length })) + 10;
     var legend = svg.selectAll(".legend")
         .data(color.domain().slice())
         .enter().append("g")
@@ -165,7 +146,7 @@ function stackedHistOrdinal(data, divToShow, title) {
         .attr("transform", function (d, i) {
             maxLegendWidth = (i % 4) * (width + 3 * (margin.left + margin.right) / 4) / 4;
             maxLegendHeight = Math.floor(i / 4) * 12;
-            return "translate(" + (maxLegendWidth - 3 * margin.left / 4) + ", " + (height + margin.top + 30 + maxLegendHeight) + ")";
+            return "translate(" + (maxLegendWidth - 3 * margin.left / 4) + ", " + (height + margin.top + yaxisheight + maxLegendHeight) + ")";
         });
 
     legend.append("rect")
@@ -182,19 +163,19 @@ function stackedHistOrdinal(data, divToShow, title) {
             return d;
         });
 
-    var statlegend = svg.selectAll(".statlegend")
-        .data(statistics)
-        .enter()
-        .append("g")
-        .attr("class", "statlegend")
-        .attr("transform", function (d, i) {
-            return "translate(" + (width) + ", " + (margin.top + (i - 1) * 15) + ")";
-        });
-    statlegend.append("text")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("class", "stattext")
-        .text(function (d) {
-            return d.type + "=" + d.val;
-        });
+    // var statlegend = svg.selectAll(".statlegend")
+    //     .data(statistics)
+    //     .enter()
+    //     .append("g")
+    //     .attr("class", "statlegend")
+    //     .attr("transform", function (d, i) {
+    //         return "translate(" + (width) + ", " + (margin.top + (i - 1) * 15) + ")";
+    //     });
+    // statlegend.append("text")
+    //     .attr("x", 0)
+    //     .attr("y", 0)
+    //     .attr("class", "stattext")
+    //     .text(function (d) {
+    //         return d.type + "=" + d.val;
+    //     });
 }
