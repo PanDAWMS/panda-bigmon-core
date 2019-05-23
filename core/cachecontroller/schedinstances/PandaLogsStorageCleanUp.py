3072343d75336284dc9418cfd551ab5d1a7e6962
Sergey Padolski <spodolsky@mail.ru>
2019-05-23 09:58:04 -0400

    updated age condition for logs cleaning up to 2 days

--- core/cachecontroller/schedinstances/PandaLogsStorageCleanUp.py
+++ core/cachecontroller/schedinstances/PandaLogsStorageCleanUp.py
@@ -13,6 +13,6 @@ class PandaLogsStorageCleanUp(BaseTasksProvider):
         for r, d, f in os.walk(PANDA_LOGGER_PATH):
             for dir in d:
                 dirpath = os.path.join(r, dir)
-                if ((time.time() - os.path.getctime(dirpath)) / 60 / 60 / 24 > 3):
+                if ((time.time() - os.path.getctime(dirpath)) / 60 / 60 / 24 > 2):
                     shutil.rmtree(dirpath)
         self.logger.info("PandaLogsStorageCleanUp finished")
