36f5725fda75c0073da1453556ca89531c7c1612
tkorchug <tatiana.korchuganova@cern.ch>
2019-08-01 12:32:04 +0200

    revert changes in JS libs

--- core/static/js/datatables/DataTables-1.10.13/js/jquery.dataTables.js
+++ core/static/js/datatables/DataTables-1.10.13/js/jquery.dataTables.js
@@ -5774,7 +5774,7 @@
 		for ( var i=0, ien=settings.aoData.length ; i<ien ; i++ ) {
 			s = _fnGetCellData( settings, i, colIdx, 'display' )+'';
 			s = s.replace( __re_html_remove, '' );
-			s = s.replace( /   /g, ' ' );
+			s = s.replace( /&nbsp;/g, ' ' );
 	
 			if ( s.length > max ) {
 				max = s.length;
