51de666a0d1ff5a889285230667edf5e9d8090bc
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2019-09-10 17:28:30 +0200

    Added time window change panel + foundation datepicker package installed

--- core/upd/static/upd/js/app.js
+++ core/upd/static/upd/js/app.js
@@ -2,3 +2,12 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 
 $(document).foundation();
+
+$(function(){
+  $('.fdatetimepicker').fdatepicker({
+		format: 'yyyy-mm-dd hh:ii:ss',
+		disableDblClickSelection: true,
+		language: 'vi',
+		pickTime: true
+	});
+});
