f1d8beec12f30096b2a56d991e75b7a397a75971
SergeyPod <sergey.padolski@gmail.com>
2020-07-01 12:01:54 -0400

    updated/invalidated settings

--- core/mlflowdynamic/openshiftcontroller.py
+++ core/mlflowdynamic/openshiftcontroller.py
@@ -6,10 +6,7 @@ import random
 from pprint import pprint
 from kubernetes.client.rest import ApiException
 
-
-AUTH_TOKEN="eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJ0ZXN0LW1sZmxvdy1iaWdwYW5kYSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJiaWdwYW5kYS10b2tlbi04bjdtOCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJiaWdwYW5kYSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjgwN2NhZDIxLWI1NGQtMTFlYS04MzQ4LTAyMTYzZTAxODBmZCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDp0ZXN0LW1sZmxvdy1iaWdwYW5kYTpiaWdwYW5kYSJ9.YgKzH5Lbax_PyC9hHYGnX7JUaEFHpFO_YHcMbTY3M0jrwxpfnURSPH3k8wNrb_8_U5_1qqqOJMfJ2PNkwU6QFxlES11twCda4Oj3uDXcmzBrgsnoex5GfwkZHV3J1IppRLLG2quV8PPIjW8no6GCjuAHYTczjfYhDp2lz9b0JqjqaZByc8rka7BV8qvn5XSbSujIkXEpZ4vFSj5AUeMp2jaks4ZTt55ZsAVrMT09TMWENctuFlXJas89HWzfLoWTB2hu4hry-X_8mZ1F53TmXeYNnt5UqPrQwKgTaJ9U3LikEx0AiBZ8wGXjgLHbISaxubTxauf3-iZX7NloldIOzg"
-ENDPOINT="openshift-dev.cern.ch"
-NAMESPACE="test-mlflow-bigpanda"
+from core.settings.local import OC_TOKEN, OC_ENDPOINT, OC_NAMESPACE
 
 RANDOM_STRING = ''.join(random.choices(string.ascii_uppercase +
                              string.digits, k = 7))
@@ -17,11 +14,10 @@ RANDOM_STRING = ''.join(random.choices(string.ascii_uppercase +
 
 def registerConfigMap():
     configuration = client.Configuration()
-    configuration.api_key['authorization'] = AUTH_TOKEN
+    configuration.api_key['authorization'] = OC_TOKEN
     configuration.api_key_prefix['authorization'] = 'Bearer'
-    configuration.host = ENDPOINT
+    configuration.host = OC_ENDPOINT
     api_instance= client.CoreV1Api(client.ApiClient(configuration))
-    namespace = NAMESPACE
     body = client.V1ConfigMap()
     body.api_version = 'v1'
     body.data = {
@@ -38,7 +34,7 @@ def registerConfigMap():
     }
 
     try:
-        api_response = api_instance.create_namespaced_config_map(namespace, body)
+        api_response = api_instance.create_namespaced_config_map(OC_NAMESPACE, body)
         pprint(api_response)
     except ApiException as e:
         print("Exception when calling CoreV1Api->create_namespaced_config_map: %s\n" % e)
