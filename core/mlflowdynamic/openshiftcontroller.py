492b512a9dc43b9c9fa483e634eeefa14bb78a6e
SergeyPod <sergey.padolski@gmail.com>
2020-07-06 15:59:15 -0400

    updated nginx configuration

--- core/mlflowdynamic/openshiftcontroller.py
+++ core/mlflowdynamic/openshiftcontroller.py
@@ -33,11 +33,11 @@ class occlicalls:
         return self.INSTANCE
 
     def openshift_actions(self):
-        time.sleep(5)
+        #time.sleep(5)
         resp = self.openshift_action("06-deploymentconfig.yml", 'apps.openshift.io/v1', 'DeploymentConfig')
-        time.sleep(5)
+        #time.sleep(5)
         resp = self.openshift_action("07-nginx-svc.yml", 'v1', 'Service')
-        time.sleep(5)
+        #time.sleep(5)
         resp = self.openshift_action("08-route.yml", 'v1', 'Route')
 
 
@@ -51,7 +51,7 @@ class occlicalls:
 
     def register_config_map(self):
         data = {
-            "bigpanda-redirect.conf": "server {\n  listen       8080 default_server;\n\n  server_tokens off; \n\n  location / {\n    rewrite ^/" + self.INSTANCE + "/(.*) /\$1 break;\n    proxy_pass http://127.0.0.1:32768/; \n    proxy_redirect     off;\n    proxy_set_header   Host \$host;\n  }\n  access_log  /dev/stdout;\n  error_log   /dev/stdout;\n\n  error_page   500 502 503 504  /50x.html;\n  location = /50x.html {\n  root   /usr/share/nginx/html;\n  }\n}\n"
+            "bigpanda-redirect.conf": "server {\n  listen       8080 default_server;\n\n  server_tokens off; \n\n  location / {\n    rewrite ^/" + self.INSTANCE + "/(.*) /$1 break;\n    proxy_pass http://127.0.0.1:32768/; \n    proxy_redirect     off;\n    proxy_set_header   Host $host;\n  }\n  access_log  /dev/stdout;\n  error_log   /dev/stdout;\n\n  error_page   500 502 503 504  /50x.html;\n  location = /50x.html {\n  root   /usr/share/nginx/html;\n  }\n}\n"
         }
         labels = {
             "name": "nginx-redirection-{instance_path}".format(instance_path=self.INSTANCE),
@@ -104,7 +104,7 @@ class occlicalls:
 
 if __name__ == "__main__":
     ocwrap = occlicalls()
-    time.sleep(5)
+    #time.sleep(5)
     ocwrap.register_config_map()
     ocwrap.openshift_actions()
 
