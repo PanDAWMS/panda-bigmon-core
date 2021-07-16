99df34d73a0306a2041c53178f849e9e429baf4d
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2021-07-16 09:16:45 +0200

    iDDS | update of iDDS constants

--- core/iDDS/constants.py
+++ core/iDDS/constants.py
@@ -28,6 +28,7 @@ class Sections:
     Transporter = 'transporter'
     Carrier = 'carrier'
     Conductor = 'conductor'
+    Consumer = 'consumer'
 
 
 class HTTP_STATUS_CODE:
@@ -47,7 +48,51 @@ class HTTP_STATUS_CODE:
     InternalError = 500
 
 
-class WorkStatus(Enum):
+class IDDSEnum(Enum):
+    def to_dict(self):
+        ret = {'class': self.__class__.__name__,
+               'module': self.__class__.__module__,
+               'attributes': {}}
+        for key, value in self.__dict__.items():
+            if not key.startswith('__'):
+                if key == 'logger':
+                    value = None
+                if value and hasattr(value, 'to_dict'):
+                    value = value.to_dict()
+                ret['attributes'][key] = value
+        return ret
+
+    @staticmethod
+    def is_class(d):
+        if d and isinstance(d, dict) and 'class' in d and 'module' in d and 'attributes' in d:
+            return True
+        return False
+
+    @staticmethod
+    def load_instance(d):
+        module = __import__(d['module'], fromlist=[None])
+        cls = getattr(module, d['class'])
+        if issubclass(cls, Enum):
+            impl = cls(d['attributes']['_value_'])
+        else:
+            impl = cls()
+        return impl
+
+    @staticmethod
+    def from_dict(d):
+        if IDDSEnum.is_class(d):
+            impl = IDDSEnum.load_instance(d)
+            for key, value in d['attributes'].items():
+                if key == 'logger':
+                    continue
+                if IDDSEnum.is_class(value):
+                    value = IDDSEnum.from_dict(value)
+                setattr(impl, key, value)
+            return impl
+        return d
+
+
+class WorkStatus(IDDSEnum):
     New = 0
     Ready = 1
     Transforming = 2
@@ -58,9 +103,17 @@ class WorkStatus(Enum):
     ToCancel = 7
     Cancelling = 8
     Cancelled = 9
+    ToSuspend = 10
+    Suspending = 11
+    Suspended = 12
+    ToResume = 13
+    Resuming = 14
+    ToExpire = 15
+    Expiring = 16
+    Expired = 17
 
 
-class RequestStatus(Enum):
+class RequestStatus(IDDSEnum):
     New = 0
     Ready = 1
     Transforming = 2
@@ -71,14 +124,24 @@ class RequestStatus(Enum):
     ToCancel = 7
     Cancelling = 8
     Cancelled = 9
+    ToSuspend = 10
+    Suspending = 11
+    Suspended = 12
+    ToResume = 13
+    Resuming = 14
+    ToExpire = 15
+    Expiring = 16
+    Expired = 17
+    ToFinish = 18
+    ToForceFinish = 19
 
 
-class RequestLocking(Enum):
+class RequestLocking(IDDSEnum):
     Idle = 0
     Locking = 1
 
 
-class WorkprogressStatus(Enum):
+class WorkprogressStatus(IDDSEnum):
     New = 0
     Ready = 1
     Transforming = 2
@@ -89,14 +152,24 @@ class WorkprogressStatus(Enum):
     ToCancel = 7
     Cancelling = 8
     Cancelled = 9
+    ToSuspend = 10
+    Suspending = 11
+    Suspended = 12
+    ToResume = 13
+    Resuming = 14
+    ToExpire = 15
+    Expiring = 16
+    Expired = 17
+    ToFinish = 18
+    ToForceFinish = 19
 
 
-class WorkprogressLocking(Enum):
+class WorkprogressLocking(IDDSEnum):
     Idle = 0
     Locking = 1
 
 
-class RequestType(Enum):
+class RequestType(IDDSEnum):
     Workflow = 0
     EventStreaming = 1
     StageIn = 2
@@ -106,17 +179,19 @@ class RequestType(Enum):
     Other = 99
 
 
-class TransformType(Enum):
+class TransformType(IDDSEnum):
     Workflow = 0
     EventStreaming = 1
     StageIn = 2
     ActiveLearning = 3
     HyperParameterOpt = 4
     Derivation = 5
+    Processing = 6
+    Actuating = 7
     Other = 99
 
 
-class TransformStatus(Enum):
+class TransformStatus(IDDSEnum):
     New = 0
     Ready = 1
     Transforming = 2
@@ -127,27 +202,37 @@ class TransformStatus(Enum):
     ToCancel = 7
     Cancelling = 8
     Cancelled = 9
+    ToSuspend = 10
+    Suspending = 11
+    Suspended = 12
+    ToResume = 13
+    Resuming = 14
+    ToExpire = 15
+    Expiring = 16
+    Expired = 17
+    ToFinish = 18
+    ToForceFinish = 19
 
 
-class TransformLocking(Enum):
+class TransformLocking(IDDSEnum):
     Idle = 0
     Locking = 1
 
 
-class CollectionType(Enum):
+class CollectionType(IDDSEnum):
     Container = 0
     Dataset = 1
     File = 2
     PseudoDataset = 3
 
 
-class CollectionRelationType(Enum):
+class CollectionRelationType(IDDSEnum):
     Input = 0
     Output = 1
     Log = 2
 
 
-class CollectionStatus(Enum):
+class CollectionStatus(IDDSEnum):
     New = 0
     Updated = 1
     Processing = 2
@@ -156,20 +241,29 @@ class CollectionStatus(Enum):
     SubClosed = 5
     Failed = 6
     Deleted = 7
+    Cancelled = 8
+    Suspended = 9
 
 
-class CollectionLocking(Enum):
+class CollectionLocking(IDDSEnum):
     Idle = 0
     Locking = 1
 
 
-class ContentType(Enum):
+class ContentType(IDDSEnum):
     File = 0
     Event = 1
     PseudoContent = 2
 
 
-class ContentStatus(Enum):
+class ContentRelationType(IDDSEnum):
+    Input = 0
+    Output = 1
+    Log = 2
+    InputDependency = 3
+
+
+class ContentStatus(IDDSEnum):
     New = 0
     Processing = 1
     Available = 2
@@ -178,19 +272,21 @@ class ContentStatus(Enum):
     Lost = 5
     Deleted = 6
     Mapped = 7
+    FakeAvailable = 8
+    Missing = 9
 
 
-class ContentLocking(Enum):
+class ContentLocking(IDDSEnum):
     Idle = 0
     Locking = 1
 
 
-class GranularityType(Enum):
+class GranularityType(IDDSEnum):
     File = 0
     Event = 1
 
 
-class ProcessingStatus(Enum):
+class ProcessingStatus(IDDSEnum):
     New = 0
     Submitting = 1
     Submitted = 2
@@ -201,40 +297,94 @@ class ProcessingStatus(Enum):
     Cancel = 7
     FinishedOnStep = 8
     FinishedOnExec = 9
-    TimeOut = 10
-    FinishedTerm = 11
+    FinishedTerm = 10
+    SubFinished = 11
+    ToCancel = 12
+    Cancelling = 13
+    Cancelled = 14
+    ToSuspend = 15
+    Suspending = 16
+    Suspended = 17
+    ToResume = 18
+    Resuming = 19
+    ToExpire = 20
+    Expiring = 21
+    Expired = 22
+    TimeOut = 23
+    ToFinish = 24
+    ToForceFinish = 25
 
 
-class ProcessingLocking(Enum):
+class ProcessingLocking(IDDSEnum):
     Idle = 0
     Locking = 1
 
 
-class MessageType(Enum):
+class MessageType(IDDSEnum):
     StageInFile = 0
     StageInCollection = 1
-    ActiveLearningFile = 2
-    ActiveLearningCollection = 3
-    HyperParameterOptFile = 4
-    HyperParameterOptCollection = 5
-    UnknownFile = 98
-    UnknownCollection = 99
+    StageInWork = 2
+    ActiveLearningFile = 3
+    ActiveLearningCollection = 4
+    ActiveLearningWork = 5
+    HyperParameterOptFile = 6
+    HyperParameterOptCollection = 7
+    HyperParameterOptWork = 8
+    ProcessingFile = 9
+    ProcessingCollection = 10
+    ProcessingWork = 11
+    HealthHeartbeat = 12
+    IDDSCommunication = 13
+    UnknownFile = 97
+    UnknownCollection = 98
+    UnknownWork = 99
 
 
-class MessageStatus(Enum):
+class MessageTypeStr(IDDSEnum):
+    StageInFile = 'file_stagein'
+    StageInCollection = 'collection_stagein'
+    StageInWork = 'work_stagein'
+    ActiveLearningFile = 'file_activelearning'
+    ActiveLearningCollection = 'collection_activelearning'
+    ActiveLearningWork = 'work_activelearning'
+    HyperParameterOptFile = 'file_hyperparameteropt'
+    HyperParameterOptCollection = 'collection_hyperparameteropt'
+    HyperParameterOptWork = 'work_hyperparameteropt'
+    ProcessingFile = 'file_processing'
+    ProcessingCollection = 'collection_processing'
+    ProcessingWork = 'work_processing'
+    HealthHeartbeat = 'health_heartbeat'
+    IDDSCommunication = 'idds_communication'
+    UnknownFile = 'file_unknown'
+    UnknownCollection = 'collection_unknown'
+    UnknownWork = 'work_unknown'
+
+
+class MessageStatus(IDDSEnum):
     New = 0
     Fetched = 1
     Delivered = 2
+    Failed = 3
 
 
-class MessageLocking(Enum):
+class MessageLocking(IDDSEnum):
     Idle = 0
     Locking = 1
 
 
-class MessageSource(Enum):
+class MessageSource(IDDSEnum):
     Clerk = 0
     Transformer = 1
     Transporter = 2
     Carrier = 3
     Conductor = 4
+    Rest = 5
+
+
+class MessageDestination(IDDSEnum):
+    Clerk = 0
+    Transformer = 1
+    Transporter = 2
+    Carrier = 3
+    Conductor = 4
+    Outside = 5
