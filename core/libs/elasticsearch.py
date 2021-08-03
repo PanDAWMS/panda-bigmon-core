9ae0fe910b2a578c7224934eb570013dd63a1dba
Aleksandr Alekseev <aleksandr.alekseev@cern.ch>
2021-08-03 08:31:02 +0700

    Migration to opensearch

--- core/libs/elasticsearch.py
+++ core/libs/elasticsearch.py
@@ -2,34 +2,32 @@ from core.settings.local import ES
 from elasticsearch import Elasticsearch
 
 
-def create_esatlas_connection(use_ssl=True, verify_certs=False, timeout=2000, max_retries=10,
+def create_esatlas_connection(verify_certs=True, timeout=2000, max_retries=10,
                       retry_on_timeout=True):
     """
     Create a connection to ElasticSearch cluster
     """
 
     esHost = None
-    esPort = None
     esUser = None
     esPassword = None
 
     if 'esHost' in ES:
-        esHost = ES['esHost']
-    if 'esPort' in ES:
-        esPort = ES['esPort']
+        #esHost = ES['esHost']
+        esHost = ES['esHost'][0:8] + '1' + ES['esHost'][8:]
     if 'esUser' in ES:
         esUser = ES['esUser']
     if 'esPassword' in ES:
         esPassword = ES['esPassword']
     try:
         connection = Elasticsearch(
-            [{'host': esHost, 'port': int(esPort)}],
+            ['https://{0}/es'.format(esHost)],
             http_auth=(esUser, esPassword),
-            use_ssl=use_ssl,
             verify_certs=verify_certs,
             timeout=timeout,
             max_retries=max_retries,
             retry_on_timeout=retry_on_timeout,
+            ca_certs='/etc/pki/tls/certs/ca-bundle.trust.crt'
         )
         return connection
     except Exception as ex:
