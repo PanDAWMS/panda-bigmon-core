cd81cd537820b3e18d964a658c9522cf4f5717e6
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2022-06-01 16:48:08 +0200

    cachecontroller | added cleaning up of old logs copied for ISP tool to avoid CEPHFS overflow

--- core/cachecontroller/schedinstances/PandaLogsStorageCleanUp.py
+++ core/cachecontroller/schedinstances/PandaLogsStorageCleanUp.py
@@ -1,7 +1,8 @@
 from BaseTasksProvider import BaseTasksProvider
 import logging
 import threading, os, time, shutil
-from settingscron import PANDA_LOGGER_PATH
+from settingscron import PANDA_LOGGER_PATH, MAX_LOG_AGE_DAYS, PANDA_LOGGER_PATH_ANALYTICS, MAX_LOG_AGE_DAYS_ANALYTICS
+
 
 class PandaLogsStorageCleanUp(BaseTasksProvider):
     lock = threading.RLock()
@@ -9,10 +10,17 @@ class PandaLogsStorageCleanUp(BaseTasksProvider):
 
     def processPayload(self):
         self.logger.info("PandaLogsStorageCleanUp started")
-
         for r, d, f in os.walk(PANDA_LOGGER_PATH):
             for dir in d:
                 dirpath = os.path.join(r, dir)
-                if ((time.time() - os.path.getctime(dirpath)) / 60 / 60 / 24 > 2):
+                if (time.time() - os.path.getctime(dirpath)) / 60 / 60 / 24 > MAX_LOG_AGE_DAYS:
                     shutil.rmtree(dirpath)
         self.logger.info("PandaLogsStorageCleanUp finished")
+
+        self.logger.info("PandaLogsAnalyticsStorageCleanUp started")
+        for r, d, f in os.walk(PANDA_LOGGER_PATH_ANALYTICS):
+            for dir in d:
+                dirpath = os.path.join(r, dir)
+                if (time.time() - os.path.getctime(dirpath)) / 60 / 60 / 24 > MAX_LOG_AGE_DAYS_ANALYTICS:
+                    shutil.rmtree(dirpath)
+        self.logger.info("PandaLogsAnalyticsStorageCleanUp finished")
