d39a2cabed47e04a6b4b63713194cbb84d66eca7
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2022-06-27 11:55:16 +0200

    models | lowercase table names and fields for Postgres compatibility

--- core/oauth/models.py
+++ core/oauth/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from core.settings.config import DB_SCHEMA
 
+
 class BPUser(AbstractUser):
     is_tester = models.NullBooleanField(db_column='is_tester', null=True, blank=False)
     last_login = models.DateTimeField(db_column='last_login', auto_now_add=True, blank=False)
@@ -14,21 +15,21 @@ class BPUser(AbstractUser):
 
 
 class BPUserSettings(models.Model):
-    userid = models.IntegerField(db_column='USERID', null=False)
-    page = models.CharField(db_column='PAGE', max_length=100, null=False)
-    preferences = models.CharField(db_column='PREFERENCES', max_length=4000)
+    userid = models.IntegerField(db_column='userid', null=False)
+    page = models.CharField(db_column='page', max_length=100, null=False)
+    preferences = models.CharField(db_column='preferences', max_length=4000)
 
     class Meta:
-        db_table = f'"{DB_SCHEMA}"."USER_SETTINGS"'
+        db_table = f'"{DB_SCHEMA}"."user_settings"'
 
 
 class Visits(models.Model):
-    visitId = models.BigIntegerField(primary_key=True, db_column='VISITID')
-    url = models.CharField(null=True, db_column='URL', max_length=1000)
-    time = models.DateTimeField(db_column='TIME', null=False)
-    remote = models.CharField(null=True, db_column='REMOTE', max_length=20)
-    userid = models.IntegerField(null=True, db_column='USERID', blank=True)
-    service = models.IntegerField(null=True, db_column='SERVICE', blank=True)
+    visitId = models.BigIntegerField(primary_key=True, db_column='visitid')
+    url = models.CharField(null=True, db_column='url', max_length=1000)
+    time = models.DateTimeField(db_column='time', null=False)
+    remote = models.CharField(null=True, db_column='remote', max_length=20)
+    userid = models.IntegerField(null=True, db_column='userid', blank=True)
+    service = models.IntegerField(null=True, db_column='service', blank=True)
 
     class Meta:
-        db_table= f'"{DB_SCHEMA}"."VISITS"'
+        db_table = f'"{DB_SCHEMA}"."visits"'
