eee8a1f8cee325f110ea6f02b252994c29cb509c
tmaeno <tmaeno@bnl.gov>
2022-07-18 12:49:29 +0200

    Indigo IAM

--- core/settings/config.py
+++ core/settings/config.py
@@ -37,7 +37,7 @@ if 'BIGMON_VO' in os.environ:
     MON_VO = os.environ['BIGMON_VO']
 
 ### Authentication providers
-if 'BIGMON_AUTH_PROVIDER_LIST' in os.environ:
+if 'BIGMON_AUTH_PROVIDER_LIST' in os.environ and os.environ['BIGMON_AUTH_PROVIDER_LIST']:
     BIGMON_AUTH_PROVIDER_LIST = os.environ['BIGMON_AUTH_PROVIDER_LIST'].split(',')
 
 ### DB_ROUTERS for atlas's prodtask
