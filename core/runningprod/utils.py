a671c3d12f568039046e142de8382b00eb67e18e
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2022-08-02 16:58:23 +0200

    runningprod | added links to ProdSys for hashtags

--- core/runningprod/utils.py
+++ core/runningprod/utils.py
@@ -5,6 +5,7 @@ Utils for runningProdTasks module
 
 import numpy as np
 import copy
+import logging
 from datetime import datetime
 from django.db import transaction, DatabaseError
 from core.runningprod.models import ProdNeventsHistory, RunningProdTasksModel
@@ -12,6 +13,8 @@ from core.common.models import JediTasks
 import core.constants as const
 
 from core.libs.sqlcustom import preprocess_wild_card_string
+
+_logger = logging.getLogger('bigpandamon')
 
 
 def updateView(request, query, exquery, wild_card_str):
@@ -49,6 +52,12 @@ def updateView(request, query, exquery, wild_card_str):
         query['priority__lte'] = query['currentpriority__lte']
         del query['currentpriority__gte']
         del query['currentpriority__lte']
+
+    if 'runnumber' in request.session['requestParams'] and request.session['requestParams']['runnumber']:
+        try:
+            query['runnumber'] = int(request.session['requestParams']['runnumber'])
+        except:
+            _logger.exception('Provided runnumber is not valid. It should be int')
 
     jedi_tasks_fields = [field.name for field in JediTasks._meta.get_fields() if field.get_internal_type() == 'CharField']
     running_prod_fields = (set([
