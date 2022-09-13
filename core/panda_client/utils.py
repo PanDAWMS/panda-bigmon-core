dfb0277f29b4ce482be2f8360bc26094c4dca38b
Aleksandr Alekseev <aleksandr.alekseev@cern.ch>
2022-09-13 10:39:14 +0200

    Update utils.py

--- core/panda_client/utils.py
+++ core/panda_client/utils.py
@@ -1,7 +1,5 @@
 import time, os
 
-from pandaclient import Client
-
 def get_auth_indigoiam(request):
     header = {}
     organisation = 'atlas'
@@ -24,21 +22,3 @@ def get_auth_indigoiam(request):
         header['Origin'] = organisation
 
     return header
-
-### TODO change it later
-def pandaclient_initialization(request):
-    user = request.user
-
-    if user.is_authenticated and user.social_auth is not None:
-        auth_provider = (request.user.social_auth.get()).provider
-        social = request.user.social_auth.get(provider=auth_provider)
-
-        os.environ['PANDA_AUTH_ID_TOKEN'] = social.extra_data['id_token']
-        os.environ['PANDA_AUTH'] = 'oidc'
-        os.environ['PANDA_AUTH_VO'] = 'atlas'
-
-        try:
-            c = Client()
-            print('Successful')
-        except Exception as ex:
-            print(ex)
