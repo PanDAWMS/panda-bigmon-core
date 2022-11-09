24cf84a424e98d1ba250f960d40156f535028b80
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2022-11-09 17:20:56 +0100

    Rubin support | add SLAC domain to ALLOWED_HOSTS temporarily

--- core/settings/config.py
+++ core/settings/config.py
@@ -6,17 +6,18 @@ from core import filebrowser, admin
 from core.settings.local import MY_SECRET_KEY, LOG_ROOT
 
 ALLOWED_HOSTS = [
-    ### cern.ch
+    # cern.ch
     '.cern.ch',  # Allow domain and subdomains
     '.cern.ch.',  # Also allow FQDN and subdomains
-    ### bigpanda.cern.ch
+    # bigpanda.cern.ch
     'bigpanda.cern.ch',  # Allow domain and subdomains
     'bigpanda.cern.ch.',  # Also allow FQDN and subdomains
-    ### pandawms.org
-    '.pandawms.org',  # Allow domain and subdomains
-    '.pandawms.org.',  # Also allow FQDN and subdomains
-
-    '127.0.0.1', '.localhost'
+    # @SLAC for Rubin
+    '.slac.stanford.edu',
+    '.slac.stanford.edu.',
+    # local
+    '127.0.0.1',
+    '.localhost'
 ]
 
 if 'BIGMON_HOST' in os.environ:
