e21eee4d63b39f367edc2e9891962ec9e173facc
Aleksandr Alekseev <aleksandr.alekseev@cern.ch>
2023-09-18 13:33:07 +0200

    updating the docker

--- docker/start-daemon.sh
+++ docker/start-daemon.sh
@@ -30,8 +30,10 @@ if [ ! -f /etc/grid-security/chain.pem ]; then
 fi
 
 if [ "${BIGMON_SERVICE}" == "all" ]; then
-  echo "starting bigmon http service"
+  echo "Starting bigmon http service"
   /usr/sbin/httpd
+  echo "Starting daphne service"
+  source ${BIGMON_VIRTUALENV_PATH}/bin/activate && cd ${BIGMON_WSGI_PATH} && daphne core.asgi:application -b 0.0.0.0 -p 8000
 else
   exec "$@"
 fi
