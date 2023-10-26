e8dc41d0ebe5cebe2874be14d68bdad73196ce38
Aleksandr Alekseev <aleksandr.alekseev@cern.ch>
2023-10-26 15:25:56 +0200

    Updating task live page

--- core/kafka/views.py
+++ core/kafka/views.py
@@ -20,22 +20,10 @@ def taskLivePage(request, jeditaskid):
         db_source = 'atlas'
     elif settings.DEPLOYMENT == 'ORACLE_DOMA':
         db_source = 'doma'
-    db_source = 'doma' # TODO removed it
 
     data = {
         'db_source': db_source,
         'jeditaskid': jeditaskid
     }
 
-    # archived_messages, task_message_ids_list, jobs_info_status_dict = get_es_task_status_log(db_source=db_source, jeditaskid=jeditaskid)
-    # for key, value in jobs_info_status_dict.items():
-    #     max_timestamp_object = max(value.values(), key=lambda item: item['timestamp'])
-    #     print(max_timestamp_object)
-    #
-    # if len(archived_messages) > 0:
-    #     data['archived_messages'] = archived_messages
-    # else:
-    #     data['archived_messages'] = 0
-
-    data['archived_messages'] = 0
     return render(request, 'taskLivePage.html', data, content_type='text/html')
