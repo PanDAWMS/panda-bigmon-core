31a51d027eab4461983a1169ca6d7a54b47c3d29
Alexander.Undrus <undrus@bnl.gov>
2024-05-21 21:42:55 +0200

    updates: forcing AFS or EOS storage usage (according to DB specification), improve style of timeout icon

--- core/buildmonitor/viewscomps.py
+++ core/buildmonitor/viewscomps.py
@@ -32,12 +32,12 @@ def compviewDemo(request):
     check_icon='<div class="ui-widget ui-state-check" style="display:inline-block;"> <span s\
 tyle="display:inline-block;" title="OK" class="DataTables_sort_icon css_right ui-icon ui-ico\
 n-circle-check">ICON33</span></div>'
-    clock_icon='<div class="ui-widget ui-state-hover" style="display:inline-block;"> <span s\
-tyle="display:inline-block;" title="UPDATING" class="DataTables_sort_icon css_right ui-icon \
-ui-icon-clock">ICON39</span></div>'
-    minorwarn_icon='<div class="ui-widget ui-state-highlight" style="display:inline-block;"> <s\
-pan style="display:inline-block;" title="MINOR WARNING" class="DataTables_sort_icon css_righ\
-t ui-icon ui-icon-alert">ICON34</span></div>'
+    clock_icon='<div class="ui-widget ui-state-active" style="display:inline-block;backgroun\
+d:#bc0000;"> <span style="display:inline-block;" title="UPDATING" class="DataTables_sort_ico\
+n css_right ui-icon ui-icon-clock">ICON39</span></div>'
+    minorwarn_icon='<div class="ui-widget ui-state-highlight" style="display:inline-block;">\
+ <span style="display:inline-block;" title="MINOR WARNING" class="DataTables_sort_icon css_r\
+ight ui-icon ui-icon-alert">ICON34</span></div>'
     warn_icon='<div class="ui-widget ui-state-error" style="display:inline-block;"> <span st\
 yle="display:inline-block;" title="WARNING" class="DataTables_sort_icon css_right ui-icon ui\
 -icon-lightbulb">ICON35</span></div>'
@@ -60,7 +60,7 @@ pan title="N/A" class="ui-icon ui-icon-radio-off">ICONRO</span></div>'
     di_res={'-1':clock_icon,'N/A':radiooff_icon,'0':check_icon,'1':minorwarn_icon,'2':majorwarn_icon,'3':error_icon,'10':clock_icon}
     asdd='0'
     if asdd == '0':
-     query="select * from (select to_char(j.jid),j.arch||'-'||os||'-'||comp||'-'||opt as AA, j.tstamp, n.nname as nname, r.name as RNAME, s.hname, j.buildarea, j.copyarea, r.relnstamp, j.gitbr from nightlies@ATLR.CERN.CH n inner join ( releases@ATLR.CERN.CH r inner join ( jobs@ATLR.CERN.CH j inner join jobstat@ATLR.CERN.CH s on j.jid=s.jid ) on r.nid=j.nid and r.relid=j.relid ) on n.nid=r.nid where nname ='%s' and j.tstamp between sysdate-11+1/24 and sysdate order by j.tstamp asc) where RNAME ='%s' and AA='%s'" % (nname,relname,arname)
+     query="select * from (select to_char(j.jid),j.arch||'-'||os||'-'||comp||'-'||opt as AA, j.tstamp, n.nname as nname, r.name as RNAME, s.hname, j.buildarea, j.copyarea, r.relnstamp, j.gitbr, n.ntype from nightlies@ATLR.CERN.CH n inner join ( releases@ATLR.CERN.CH r inner join ( jobs@ATLR.CERN.CH j inner join jobstat@ATLR.CERN.CH s on j.jid=s.jid ) on r.nid=j.nid and r.relid=j.relid ) on n.nid=r.nid where nname ='%s' and j.tstamp between sysdate-11+1/24 and sysdate order by j.tstamp asc) where RNAME ='%s' and AA='%s'" % (nname,relname,arname)
 #     print("Q ",query)
      new_cur.execute(query)
      reslt = new_cur.fetchall()
@@ -69,6 +69,7 @@ pan title="N/A" class="ui-icon ui-icon-radio-off">ICONRO</span></div>'
      buildareaSS='N/A'
      copyareaSS='N/A'
      gitbrSS='N/A'
+     ntype='N/A'
      relnstamp=''
      lllr=len(reslt)
      if lllr > 0:
@@ -78,6 +79,7 @@ pan title="N/A" class="ui-icon ui-icon-radio-off">ICONRO</span></div>'
         buildareaSS = rowmax[6]
         copyareaSS = rowmax[7]
         relnstamp = rowmax[8]
+        ntype = rowmax[10]
         if gitbrSS != None : gitbrSS=rowmax[9]
         tabname='compresults'
         if pjname == '*' or re.match('^all$',pjname,re.IGNORECASE) :
@@ -92,9 +94,17 @@ pan title="N/A" class="ui-icon ui-icon-radio-off">ICONRO</span></div>'
         reslt1 = new_cur.fetchall()
      relextend=relname
      if re.search('ATN',nname): relextend=relnstamp+'('+relname+')'
+     afs_top_url='atlas-computing.web.cern.ch/atlas-computing/links/distDirectory/gitwww'
+     eos_top_url='atlas-sw-www.web.cern.ch/gitwww'
+     afs_top_url1 = '//cern.ch/atlas-computing/links/distDirectory/gitwww'
+     eos_top_url1 = '//atlas-sw-www.web.cern.ch/gitwww'
      if re.search('CI',nname):
         sComm='git branch '+gitbrSS
         cmmnt='ATLAS CI %s, release %s, platform %s (on %s)<BR><span style="font-size:  smaller">%s</span>' % ( nname, relextend, arname, host, sComm )
+        afs_top_url = 'atlas-computing.web.cern.ch/atlas-computing/links/distDirectory/ci'
+        eos_top_url = 'atlas-sw-www.web.cern.ch/ciwww'
+        afs_top_url1 = '//cern.ch/atlas-computing/links/distDirectory/ci'
+        eos_top_url1 = '//atlas-sw-www.web.cern.ch/ciwww'
      else:
         cmmnt='ATLAS nightly %s, release %s, platform %s, project %s (on %s)' % ( nname, relextend, arname, pjname, host)
 #  HEADERS
@@ -109,12 +119,19 @@ pan title="N/A" class="ui-icon ui-icon-radio-off">ICONRO</span></div>'
       if i > 10000: break
       result=row[0]
       proj=row[1]
-      nameln=row[2]
+      nameln1=row[2]
       container=row[3]
       corder=row[4]
       i_result=di_res.get(str(result),str(result))
-      if i_result == None or i_result == "None" : i_result=radiooff_icon; 
-      row_cand=[i_result,proj,nameln,container]
+      if i_result == None or i_result == "None" : i_result=radiooff_icon;
+      nameln3=nameln1
+      if ntype == 'eos':
+          nameln2 = re.sub(afs_top_url, eos_top_url, nameln1)
+          nameln3 = re.sub(afs_top_url1, eos_top_url1, nameln2)
+      elif ntype == 'afs':
+          nameln2 = re.sub(eos_top_url, afs_top_url, nameln1)
+          nameln3 = re.sub(eos_top_url1, afs_top_url1, nameln2)
+      row_cand=[i_result,proj,nameln3,container]
       rows_s.append(row_cand)
 
     data={"nightly": nname, "rel": relname, "ar": arname, "proj": pjname, 'viewParams': request.session['viewParams'],'rows_s':json.dumps(rows_s, cls=DateEncoder)}
