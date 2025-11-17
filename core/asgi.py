0997e0f6038add2b3cd5a7163b703330138bd051
Aleksandr Alekseev <aleksandr.alekseev@cern.ch>
2025-11-17 11:03:10 +0100

    1. Migration to Python 3.13 (fix ASGI) 2. ATLASPANDA-1583

--- core/asgi.py
+++ core/asgi.py
@@ -14,3 +14,10 @@ DEPLOYMENT = os.environ.get('BIGMON_DEPLOYMENT', None)
 
 import django
 django.setup()
+
+from django.core.asgi import get_asgi_application
+
+application = ProtocolTypeRouter({
+  'http': get_asgi_application(),
+  'websocket': AuthMiddlewareStack(URLRouter())
+})
